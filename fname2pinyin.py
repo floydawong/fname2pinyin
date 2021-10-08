c9d275f2d3136fee8c9bf100611d4682af67e514
floyda <floyda@163.com>
2021-10-08 14:27:04 +0800

    过滤并显示重复的文件

--- fname2pinyin.py
+++ fname2pinyin.py
@@ -8,8 +8,10 @@ from xpinyin import Pinyin
 
 pinyin = Pinyin()
 
+WARNING_FILE_NAME = 'warning_duplication.txt'
 
-def __tidy_name(name):
+
+def _tidy_name(name):
     invalid_char = u" !\"#$%&'()*+,-./:;<=>?@[\\]^_{|}—‘’“”…、。《》【】！（），：；？￥"
     common_char = u"1234567890"
     fname = ""
@@ -38,27 +40,56 @@ def __tidy_name(name):
     return fname.lower()
 
 
-def __walk_dir(dir_name):
+duplication_cache = {}
+class DuplicationTypeConst:
+    ORGIN_DUPLICATION = "ORGIN_DUPLICATION"
+    PINYIN_DUPLICATION = "PINYIN_DUPLICATION"
+
+
+def _check_duplication_files(tag, content):
+    global duplication_cache
+    cache =  duplication_cache.get(tag, {})
+    duplication_cache[tag] = cache
+    _tmp = cache.get(content, 0)
+    cache[content] = _tmp + 1
+
+
+def output_duplication_files():
+    global duplication_cache
+    for key, value in duplication_cache.items():
+
+        def _write_result(content):
+            with open(WARNING_FILE_NAME, 'a+', encoding='utf-8') as fp:
+                fp.write(key + ': ' + content + '\n')
+
+        for fname in value:
+            if value[fname] > 1:
+                _write_result(fname)
+
+
+def _walk_dir(dir_name):
     map_list = []
 
     for path, dirs, files in os.walk(dir_name):
 
         for dname in dirs:
             target_path = os.path.join(path, dname)
-            dname = __tidy_name(dname)
+            dname = _tidy_name(dname)
             name_pinyin = pinyin.get_pinyin(dname, "_")
             target_pinyin = os.path.join(path, name_pinyin)
             map_list.append([target_path, target_pinyin])
 
         for fname in files:
+            _check_duplication_files(DuplicationTypeConst.ORGIN_DUPLICATION, fname)
             target_path = os.path.join(path, fname)
             if fname.find(".") != -1:
                 suffix = fname.split(".")[-1]
                 fname = fname.replace("." + suffix, "")
-                fname = __tidy_name(fname)
+                fname = _tidy_name(fname)
                 name_pinyin = pinyin.get_pinyin(fname, "_") + "." + suffix
             else:
                 name_pinyin = pinyin.get_pinyin(fname, "_")
+            _check_duplication_files(DuplicationTypeConst.PINYIN_DUPLICATION, name_pinyin)
             target_pinyin = os.path.join(path, name_pinyin)
             map_list.append([target_path, target_pinyin])
 
@@ -66,11 +97,12 @@ def __walk_dir(dir_name):
 
 
 def translate_to_pinyin(target_dir="./utest", out_dir="./out"):
+    """ 翻译目标路径内的文件到输出目录 """
     if os.path.exists(out_dir):
         shutil.rmtree(out_dir)
     shutil.copytree(target_dir, out_dir)
 
-    map_list = __walk_dir(out_dir)
+    map_list = _walk_dir(out_dir)
     while len(map_list) > 0:
         data = map_list.pop()
         if not os.path.exists(data[1]):
@@ -78,7 +110,8 @@ def translate_to_pinyin(target_dir="./utest", out_dir="./out"):
 
 
 def translate_cover_pinyin(target_dir):
-    map_list = __walk_dir(target_dir)
+    """ 翻译目标路径内的文件 """
+    map_list = _walk_dir(target_dir)
     while len(map_list) > 0:
         data = map_list.pop()
         os.rename(data[0], data[1])
@@ -86,7 +119,7 @@ def translate_cover_pinyin(target_dir):
 
 def main():
     translate_to_pinyin('./res', './out')
-    # translate_cover_pinyin("../res")
+    output_duplication_files()
 
 
 if __name__ == "__main__":
